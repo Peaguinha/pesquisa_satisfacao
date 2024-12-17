0a14b37977a0277de716e128d6679a652decff02
Pedro Henrique de Almeida Peixoto <peixotopedro815@hotmail.com>
2024-12-16 22:15:15 -0300

    edição no service

--- src/controllers/controller_satisfacao.js
+++ src/controllers/controller_satisfacao.js
@@ -1,36 +1,4 @@
 import { imprimir } from "../services/enviar_planilha.js";
-
-export async function createdata(req, res) {
-    
-    let {
-        nome,
-        telefone,
-        avaliacao_atendimento,
-        nota_atendimento,
-        indicacao_atendimento,
-        nota_retorno
-    } = req.body;
-
-    try { 
-        const resultado = await imprimir(
-            nome,
-            telefone,
-            avaliacao_atendimento,
-            nota_atendimento,
-            indicacao_atendimento,
-            nota_retorno
-        );
-
-        if (resultado === true) {
-            res.status(200).json({ message: "Deu certo!" });
-        } else {
-            res.status(400).json({ message: "Deu ruim!" });
-        }
-    } catch (error) {
-        res.status(500).json({ message: `Erro de servidor: ${error.message}` });
-    }
-}
-
 import { adicionarNaPlanilha } from "../services/google_sheets_service.js";
 
 export async function createdata(req, res) {
