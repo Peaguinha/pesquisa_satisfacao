d247a91806dec6104ed81da11064a47b82585f55
Pedro Henrique de Almeida Peixoto <peixotopedro815@hotmail.com>
2024-12-16 23:16:27 -0300

    importação do path no index.js

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 import express from 'express';
-import routes from './src/routes/routes.js'; 
+import path from 'path'; 
+import routes from './src/routes/routes.js'; // Importando as rotas
 
 const app = express();
 
-// Middleware 
+// Middleware para servir o favicon
+app.use('/favicon.ico', express.static(path.join(__dirname, 'public', 'favicon.ico')));
+
+// Middleware para tratar requisições no formato JSON
 app.use(express.json());
 
 // Definindo a rota principal
 app.use('/api', routes); // As rotas estarão no caminho /api
 
-
+// Iniciando o servidor na porta 4000
 app.listen(4000, () => {
   console.log('Servidor rodando na porta 4000');
 });
-
-// Middleware para servir o favicon
-app.use('/favicon.ico', express.static(path.join(__dirname, 'public', 'favicon.ico')));
 
 export default app; // Exporta o app para testes, caso necessário
